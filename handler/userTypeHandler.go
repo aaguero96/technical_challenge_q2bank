a32bc6f03edcebf0aa68fa6288deb56d4d838870
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 11:23:35 -0300

    feat: create userTypeHandler

---
+++ handler/userTypeHandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/aaguero96/technical_challenge_q2bank/service/userTypeService"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/swag/example/celler/httputil"
+)
+
+type userTypeHandler struct {
+	userTypeService userTypeService.UserTypeService
+}
+
+func NewUserTypeHandler(userTypeService userTypeService.UserTypeService) userTypeHandler {
+	return userTypeHandler{
+		userTypeService: userTypeService,
+	}
+}
+
+func (uth userTypeHandler) GetAll(ctx *gin.Context) {
+	userTypes, err := uth.userTypeService.GetAll()
+	if err != nil {
+		httputil.NewError(ctx, http.StatusInternalServerError, err)
+	}
+
+	ctx.JSON(http.StatusOK, userTypes)
+}
