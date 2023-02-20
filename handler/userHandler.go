7e5a2df55009395d5089c8a7d5729a48606109e7
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 11:23:47 -0300

    feat: create userHandler

---
+++ handler/userHandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/aaguero96/technical_challenge_q2bank/service/userService"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/swag/example/celler/httputil"
+)
+
+type userHandler struct {
+	userService userService.UserService
+}
+
+func NewUserHandler(userService userService.UserService) userHandler {
+	return userHandler{
+		userService: userService,
+	}
+}
+
+func (uh userHandler) GetAll(ctx *gin.Context) {
+	users, err := uh.userService.GetAll()
+	if err != nil {
+		httputil.NewError(ctx, http.StatusInternalServerError, err)
+	}
+
+	ctx.JSON(http.StatusOK, users)
+}
