136535da4419aa48c13c59b991c844decde964d1
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 12:43:53 -0300

    feat: create route /user_types

---
+++ routes/userTypes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/aaguero96/technical_challenge_q2bank/handler/userTypeHandler"
+	"github.com/gin-gonic/gin"
+)
+
+func NewUserTypeRoutes(rg *gin.RouterGroup, uth userTypeHandler.UserTypeHandler) {
+	users := rg.Group("/user_types")
+
+	users.GET("/", uth.GetAll)
+}
