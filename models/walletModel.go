346fd20fa4638c38cfb8e22b4f3674fbea270059
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 10:54:15 -0300

    fix: change type of amount

--- models/walletModel.go
+++ models/walletModel.go
@@ -1,8 +1,8 @@
 package models
 
 type WalletModel struct {
-	WalletID int `gorm:"column:wallet_id,primary_key"`
-	Amount   int `gorm:"column:amount"`
+	WalletID int     `gorm:"column:wallet_id,primary_key"`
+	Amount   float64 `gorm:"column:amount"`
 }
 
 func (WalletModel) TableName() string {
