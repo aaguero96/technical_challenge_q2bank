9665519b39b19301c3efb520e75a0e50317e793b
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 09:03:28 -0300

    feat: create seeder to wallet

---
+++ migrate/seeder/walletSeeder.go
@@ -0,0 +1,16 @@
+package seeder
+
+import (
+	"github.com/aaguero96/technical_challenge_q2bank/models"
+)
+
+var WalletSeeder = []models.WalletModel{
+	{
+		WalletID: 1,
+		Amount:   10000,
+	},
+	{
+		WalletID: 2,
+		Amount:   20000,
+	},
+}
