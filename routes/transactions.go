b7f55589e62ee70e8bcfeaa75223388440d5d4cd
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 12:44:11 -0300

    feat: create route /transacrions

---
+++ routes/transactions.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/aaguero96/technical_challenge_q2bank/handler/transactionHandler"
+	"github.com/gin-gonic/gin"
+)
+
+func NewTransactionRoutes(rg *gin.RouterGroup, th transactionHandler.TransactionHandler) {
+	users := rg.Group("/transactions")
+
+	users.GET("/", th.GetAll)
+}
