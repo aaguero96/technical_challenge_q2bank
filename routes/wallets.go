39750f30ac68dafeac564d3d979b2e3cae6438cb
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-20 12:44:18 -0300

    feat: create route /wallets

---
+++ routes/wallets.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/aaguero96/technical_challenge_q2bank/handler/walletHandler"
+	"github.com/gin-gonic/gin"
+)
+
+func NewWalletRoutes(rg *gin.RouterGroup, wh walletHandler.WalletHandler) {
+	users := rg.Group("/wallets")
+
+	users.GET("/", wh.GetAll)
+}
