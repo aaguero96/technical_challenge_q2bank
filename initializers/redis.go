5ea61d2817516dad7e45b3075eff8563b5ced699
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-21 08:23:24 -0300

    lint: remove unnused fmt

--- initializers/redis.go
+++ initializers/redis.go
@@ -19,8 +19,6 @@ func ConnectRedisClient() {
 	}
 	Client = redis.NewClient(&clientConfig)
 
-	fmt.Println(os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"), os.Getenv("REDIS_PASSWORD"))
-
 	_, err = Client.Ping().Result()
 	if err != nil {
 		log.Fatal("Failed to connect to redis")
