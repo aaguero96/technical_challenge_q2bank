a57ab0267e0a813ee21b41f1acbe9076e3b55528
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-21 11:25:37 -0300

    feat: add autoincrement in models

--- models/walletModel.go
+++ models/walletModel.go
@@ -1,7 +1,7 @@
 package models
 
 type WalletModel struct {
-	WalletID int     `gorm:"column:wallet_id;primaryKey"`
+	WalletID int     `gorm:"column:wallet_id;primaryKey;autoIncrement:true"`
 	Amount   float64 `gorm:"column:amount"`
 }
 
