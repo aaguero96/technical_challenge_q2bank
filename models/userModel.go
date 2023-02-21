6c3dcccefce75a1cf2d474d7df88a8794241eed6
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-21 15:32:45 -0300

    feat: add unique key to columns

--- models/userModel.go
+++ models/userModel.go
@@ -5,9 +5,9 @@ import "gorm.io/gorm"
 type UserModel struct {
 	gorm.Model
 	Name           string `gorm:"column:name"`
-	RegisterNumber int64  `gorm:"column:register_number"`
-	RegisterTypeID int    `gorm:"column:register_type_id"`
-	Email          string `gorm:"column:email"`
+	RegisterNumber int64  `gorm:"column:register_number;index:register,unique"`
+	RegisterTypeID int    `gorm:"column:register_type_id;index:register,unique"`
+	Email          string `gorm:"column:email;unique"`
 	WalletID       int    `gorm:"column:wallet_id"`
 	UserTypeID     int    `gorm:"column:user_type_id"`
 	Password       string `gorm:"password"`
