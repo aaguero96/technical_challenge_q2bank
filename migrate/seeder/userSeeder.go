ef71a0ec3d10e6c996449986d6ac688ed5f1aa95
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-21 22:47:42 -0300

    fix: change model to hash password

--- migrate/seeder/userSeeder.go
+++ migrate/seeder/userSeeder.go
@@ -12,7 +12,7 @@ var UserSeeder = []models.UserModel{
 		Email:          "email1@testmail.com",
 		WalletID:       1,
 		UserTypeID:     1,
-		Password:       "123456789",
+		Password:       "$2a$10$16yn5uLLPNhTcalc4XWDUuEgq.uiKywVEMa9SuM7KBN6iVGzMIYui", // without hash: Def4!t*1
 	},
 	{
 		Name:           "name_2_storekeeper",
@@ -21,6 +21,6 @@ var UserSeeder = []models.UserModel{
 		Email:          "email2@testmail.com",
 		WalletID:       2,
 		UserTypeID:     2,
-		Password:       "987654321",
+		Password:       "$2a$10$WGJoxCGpj.rAm3CCU3z8pOXdgm4VCZhsOkrGIj7f2O2/DLwYupXKy", // without hash: Def4!t*2
 	},
 }
