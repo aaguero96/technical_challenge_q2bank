5b2e3db95e71cebb577d33c6268270d61bee3d17
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-23 10:52:28 -0300

    feat: apply new instances

--- main.go
+++ main.go
@@ -2,23 +2,11 @@ package main
 
 import (
 	_ "github.com/aaguero96/technical_challenge_q2bank/docs"
-	"github.com/aaguero96/technical_challenge_q2bank/handler/register_type_handler"
-	"github.com/aaguero96/technical_challenge_q2bank/handler/transaction_handler"
-	"github.com/aaguero96/technical_challenge_q2bank/handler/user_handler"
-	"github.com/aaguero96/technical_challenge_q2bank/handler/user_type_handler"
-	"github.com/aaguero96/technical_challenge_q2bank/handler/wallet_handler"
+	"github.com/aaguero96/technical_challenge_q2bank/handler"
 	"github.com/aaguero96/technical_challenge_q2bank/initializers"
-	"github.com/aaguero96/technical_challenge_q2bank/repository/register_type_repository"
-	"github.com/aaguero96/technical_challenge_q2bank/repository/transaction_repository"
-	"github.com/aaguero96/technical_challenge_q2bank/repository/user_repository"
-	"github.com/aaguero96/technical_challenge_q2bank/repository/user_type_repository"
-	"github.com/aaguero96/technical_challenge_q2bank/repository/wallet_repository"
+	"github.com/aaguero96/technical_challenge_q2bank/repository"
 	"github.com/aaguero96/technical_challenge_q2bank/routes"
-	"github.com/aaguero96/technical_challenge_q2bank/service/register_type_service"
-	"github.com/aaguero96/technical_challenge_q2bank/service/transaction_service"
-	"github.com/aaguero96/technical_challenge_q2bank/service/user_service"
-	"github.com/aaguero96/technical_challenge_q2bank/service/user_type_service"
-	"github.com/aaguero96/technical_challenge_q2bank/service/wallet_service"
+	"github.com/aaguero96/technical_challenge_q2bank/service"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -42,26 +30,9 @@ func init() {
 // @in header
 // @name Admin Authorization
 func main() {
-	// Start Repositories
-	userRepository := user_repository.NewUserRepository(initializers.DB)
-	userTypeRepository := user_type_repository.NewUserTypeRepository(initializers.DB)
-	registerTypeRepository := register_type_repository.NewRegisterTypeRepository(initializers.DB)
-	transactionRepository := transaction_repository.NewTransactionRepository(initializers.DB)
-	walletRepository := wallet_repository.NewWalletRepository(initializers.DB)
-
-	// Start Services
-	userService := user_service.NewUserService(&userRepository)
-	userTypeService := user_type_service.NewUserTypeService(userTypeRepository)
-	registerTypeService := register_type_service.NewRegisterTypeService(registerTypeRepository)
-	transactionService := transaction_service.NewTransactionService(transactionRepository, &userRepository, &walletRepository, userTypeRepository)
-	walletService := wallet_service.NewWalletService(&walletRepository)
-
-	// Start Handlers
-	userHandler := user_handler.NewUserHandler(userService)
-	userTypeHandler := user_type_handler.NewUserTypeHandler(userTypeService)
-	registerTypeHandler := register_type_handler.NewRegisterTypeHandler(registerTypeService)
-	transactionHandler := transaction_handler.NewTransactionHandler(transactionService)
-	walletHandler := wallet_handler.NewWalletHandler(walletService)
+	repositories := repository.InstanceRepositories(initializers.DB)
+	service := service.InstanceServices(repositories)
+	handlers := handler.InstanceHandlers(service)
 
 	router := gin.Default()
 
@@ -70,12 +41,12 @@ func main() {
 
 	v1 := router.Group("/v1")
 
-	routes.NewLoginRoutes(v1, userHandler)
-	routes.NewUserRoutes(v1, userHandler)
-	routes.NewUserTypeRoutes(v1, userTypeHandler)
-	routes.NewRegisterTypeRoutes(v1, registerTypeHandler)
-	routes.NewTransactionRoutes(v1, transactionHandler)
-	routes.NewWalletRoutes(v1, walletHandler)
+	routes.NewLoginRoutes(v1, handlers.UserHandler)
+	routes.NewUserRoutes(v1, handlers.UserHandler)
+	routes.NewUserTypeRoutes(v1, handlers.UserTypeHandler)
+	routes.NewRegisterTypeRoutes(v1, handlers.RegisterTypeHandler)
+	routes.NewTransactionRoutes(v1, handlers.TransactionHandler)
+	routes.NewWalletRoutes(v1, handlers.WalletHandler)
 
 	router.Run()
 }
