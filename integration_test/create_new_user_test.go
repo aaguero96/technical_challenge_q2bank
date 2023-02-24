68cd8c6c24145c3aa0b1d17a53e3676c642fc89e
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-24 11:03:41 -0300

    test: create new user ok case

--- integration_test/create_new_user_test.go
+++ integration_test/create_new_user_test.go
@@ -1,17 +1,50 @@
 package integration_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateNewUser(t *testing.T) {
+	assert := assert.New(t)
+
+	// type Body for these requests
+	type Body struct {
+		Name           string `json:"name"`
+		Email          string `json:"email"`
+		Password       string `json:"password"`
+		RegisterNumber int    `json:"register_number"`
+		RegisterTypeID int    `json:"register_type_id"`
+		UserTypeID     int    `json:"user_type_id"`
+	}
+
 	t.Run("Create user correctly but not accept cookies - OK CASE", func(t *testing.T) {
-		request, err := http.NewRequest("POST", "localhost:3000/v1/users", nil)
+		// body
+		body, err := json.Marshal(Body{
+			Name:           "name_0",
+			Email:          "name0@test.com",
+			Password:       "Def4!t*0",
+			RegisterNumber: 12345678900,
+			RegisterTypeID: 1,
+			UserTypeID:     1,
+		})
+		if err != nil {
+			fmt.Println("error when parsisng json")
+		}
+
+		// http request
+		request, err := http.NewRequest("POST", "http://localhost:3000/v1/users", bytes.NewBuffer(body))
 		if err != nil {
 			fmt.Println("error when request is created")
 		}
+
+		// http response
 		client := &http.Client{}
 		response, err := client.Do(request)
 		if err != nil {
@@ -19,9 +52,32 @@ func TestCreateNewUser(t *testing.T) {
 		}
 		defer response.Body.Close()
 
-		fmt.Println("response status", response.Status)
-		fmt.Println("response status code", response.StatusCode)
-		fmt.Println("response body", response.Body)
-		fmt.Println("response header", response.Header)
+		// Verify status code
+		assert.Equal(http.StatusCreated, response.StatusCode, "expected status %v, instead got %v", http.StatusCreated, response.StatusCode)
+
+		// // Verify Response
+		// type reponse
+		type responseType struct {
+			Token    string `json:"token"`
+			ExpingIn string `json:"expiring_in"`
+		}
+
+		// read response data
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("error to read response")
+		}
+
+		// verify if data has correct type
+		var data responseType
+		if err = json.Unmarshal([]byte(string(responseData)), &data); err != nil {
+			fmt.Println("data dont has correct types")
+		}
+
+		// verify if token not equal to ""
+		assert.NotEmpty(data.Token)
+
+		// verify if expiredIn is correct
+		assert.Equal("30 minutes", data.ExpingIn)
 	})
 }
