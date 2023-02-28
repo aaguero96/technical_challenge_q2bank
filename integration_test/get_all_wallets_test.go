4d37198c286536d26dfb9a81b3b724ca4c597683
aaguero96 <andre.luiz_1996@hotmail.com>
2023-02-28 09:31:46 -0300

    test: get all wallets

---
+++ integration_test/get_all_wallets_test.go
@@ -0,0 +1,45 @@
+package integration_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/aaguero96/technical_challenge_q2bank/integration_test/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAllWallets(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("Get all wallets correctly - OK CASE", func(t *testing.T) {
+		// login user to get token
+		token := utils.LoginUser("email1@testmail.com", "Def4!t*1")
+		fmt.Println(token)
+
+		// type reponse
+		type responseType struct {
+			ID     int     `json:"id"`
+			Amount float64 `json:"amount"`
+		}
+
+		// response
+		status, data, _ := utils.EndpointResponse[[]responseType](
+			utils.EndpointRequest{
+				Body: nil,
+				Header: utils.EndpointHeader{
+					BearerToken: "Bearer " + token,
+				},
+			},
+			"GET",
+			utils.BASE_URL+"/v1/wallets",
+		)
+
+		// assertions
+		assert.Equal(http.StatusOK, status)
+		assert.Equal([]responseType{
+			{ID: 1, Amount: 1000},
+			{ID: 2, Amount: 2000},
+		}, data)
+	})
+}
